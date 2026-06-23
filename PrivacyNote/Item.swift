f278fa3c97651c2c30a78762a42aaab41dab0d66
djenkjilmazz-tech <djenkjilmazz@gmail.com>
2026-06-23 15:37:09 +0300

    Initial Commit

---
+++ PrivacyNote/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PrivacyNote
+//
+//  Created by Cenk Yilmaz on 23.06.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PrivacyNoteTests/PrivacyNoteTests.swift
@@ -0,0 +1,18 @@
+//
+//  PrivacyNoteTests.swift
+//  PrivacyNoteTests
+//
+//  Created by Cenk Yilmaz on 23.06.2026.
+//
+
+import Testing
+
+struct PrivacyNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
